705d392d593cbb27fe4ef9aae962bad22bdf055c
Zoosewu <zoosewup@gmail.com>
2023-11-29 17:41:06 +0800

    no change
    Signed-off-by: Zoosewu <zoosewup@gmail.com>

--- src/components/layout/layout.test.tsx
+++ src/components/layout/layout.test.tsx
@@ -10,6 +10,7 @@ describe('test layout', () => {
 
     const headerElement = container.querySelector('a.navbar-brand')
     expect(headerElement).toBeInTheDocument()
+
     const footerElement = screen.getByText(/All rights reserved\./i)
     expect(footerElement).toBeInTheDocument()
   })
