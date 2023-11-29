bf55c8a5e020234c78a2b1fe7f716c00021b7b58
Zoosewu <zoosewup@gmail.com>
2023-11-29 13:21:32 +0800

    fix lint error
    Signed-off-by: Zoosewu <zoosewup@gmail.com>

--- src/mock/server.ts
+++ src/mock/server.ts
@@ -1,4 +1,4 @@
 // src/mocks/server.ts
 import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
-const server = setupServer(...handlers)
+export const server = setupServer(...handlers)
